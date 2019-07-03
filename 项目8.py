cc798dc40dfeb3e651e82c23aa42ddc0e29e636d
loveyouforever04 <1691049547@qqcom>
2019-07-03 20:58:08 +0800

    xiang

---
+++ 项目8.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+
+# Filename : 05-if.py
+# author by : （学员lisa)
+
+# 目的:
+# 掌握使用 if 语句
+#导入random（随机数）模块
+import random
+print("\n----华丽分割线----")
+a=float(input("输入三角形第一边长 a=: "))
+b=float(input("输入三角形第二边长 b=: "))
+c=float(input("输入三角形第三边长 c=: "))
+if (a+b>c)and(a+c>b)and(b+c>a):
+    if(abs(a-b)>c)or(abs(a-c)>b)or(abs(b-c)>a):
+        pass
+    else：
+        print("错误！某两边之差大于第三边，所以无法组成三角形.")
+  else：
+        if(a==b)or(b==c)or(a==c)
+        print("正确，可以组成等边或等腰三角形")
+    else：
+        print("正确，可以组成不等边三角形")
+    else：
+        print("错误！某两边之和小于第三边，所以无法组成三角形")
