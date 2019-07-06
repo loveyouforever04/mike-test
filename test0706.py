333555cebe478a6ef9967d33050f90d88088c799
loveyouforever04 <1691049547@qqcom>
2019-07-06 14:36:10 +0800

    0706xfy做的

---
+++ test0706.py
@@ -0,0 +1,14 @@
+times=300
+for x in range (1, times):
+    #for body begin
+    print ("x-No. %d times..."%(x))
+    for y in range (1,times):
+        #for body begin
+        print ("\ty-No. %d times..."%(y))
+        for z in range(1,times):
+            #for body begin
+            print("\t\tz-No.%d times..."%(z))
+            print("\t\t\tx-y-z = %d-%d-%d" % (x,y,z))
+            #for body end
+         #for body end
+    #for body end
