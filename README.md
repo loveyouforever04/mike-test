d100bb091e883c394154a14c5aef5626a5ce5d59
loveyouforever04 <1691049547@qqcom>
2019-07-06 16:15:40 +0800

    07061xfy做的

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# mike-test
-test
+
--- 项目8.py
+++ 项目8.py
@@ -13,13 +13,13 @@ b=float(input("输入三角形第二边长 b=: "))
 c=float(input("输入三角形第三边长 c=: "))
 if (a+b>c)and(a+c>b)and(b+c>a):
     if(abs(a-b)>c)or(abs(a-c)>b)or(abs(b-c)>a):
-        pass
-    else：
-        print("错误！某两边之差大于第三边，所以无法组成三角形.")
-  else：
-        if(a==b)or(b==c)or(a==c)
-        print("正确，可以组成等边或等腰三角形")
-    else：
-        print("正确，可以组成不等边三角形")
-    else：
-        print("错误！某两边之和小于第三边，所以无法组成三角形")
+       pass
+else：
+    print("错误！某两边之差大于第三边，所以无法组成三角形.")
+elif:
+    if(a==b)or(b==c)or(a==c)
+    print("正确，可以组成等边或等腰三角形")
+else：
+    print("正确，可以组成不等边三角形")
+else：
+    print("错误！某两边之和小于第三边，所以无法组成三角形")
